0871ef5372e70766aa432d881bd57fad87608aba
Patrick <synapse.and.syntax@gmail.com>
2012-01-07 16:38:56 -0500

    function renaming

--- wagner.hs
+++ wagner.hs
@@ -92,7 +92,7 @@ updateAlignment :: Gestalt -> IO Gestalt
 updateAlignment gestalt = do { let seqs = sequences gestalt
                              ; let mis = motifIndices gestalt 
                              ; i <- randomRIO (0, length mis - 1)
-                             ; return (updateIthSequence gestalt i)
+                             ; return (patrifyIthSequence gestalt i)
                               }
                            
 
@@ -134,8 +134,8 @@ separate i seqs = (seqs !! i, removeNth seqs i)
 deparate :: Index -> a -> [a] -> [a] 
 deparate i a as = take i as ++ [a] ++ drop i as --note: inserts, doesn't replace
   
-updateIthSequence :: Gestalt -> Index -> Gestalt
-updateIthSequence gestalt i = Gestalt seqs mis'
+patrifyIthSequence :: Gestalt -> Index -> Gestalt
+patrifyIthSequence gestalt i = Gestalt seqs mis'
     where 
       seqs = sequences gestalt
       mis = motifIndices gestalt
@@ -183,9 +183,9 @@ removeNth xs n = ys ++ tail zs
 iterateN :: Int -> (a -> a) -> a -> a
 iterateN n f x = iterate f x !! n
 
-updateSweep :: Gestalt -> Gestalt
-updateSweep g | trace ("updateSweep"++ " " ++ show (motifIndices g)) False = undefined
-updateSweep g = foldl updateIthSequence g is
+patSweep :: Gestalt -> Gestalt
+patSweep g | trace ("patSweep"++ " " ++ show (motifIndices g)) False = undefined
+patSweep g = foldl patrifyIthSequence g is
   where is = (range . length . motifIndices) g
 
 ivanSweep :: Gestalt -> IO Gestalt
