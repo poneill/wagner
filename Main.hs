22262d2b17ec8d370b93271a95c8832b24c28003
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-01-18 13:23:33 -0500

    iterations now taken from command line arg

--- Main.hs
+++ Main.hs
@@ -1,9 +1,13 @@
 module Main where
 import Wagner                         
 import Utils
+import System (getArgs)
 
-main = do seqs <- readSequences "data/lexA_e_coli_120.csv"
+main = do args <- getArgs
+          let iterations = read $ head args
+          seqs <- readSequences "data/lexA_e_coli_120.csv"
           mis <- seedMotifs seqs
           let g = Gestalt seqs mis
-          g' <- iterateN 1000 (>>= sa) (return g)
+          g' <- iterateN iterations (>>= sa) (return g)
           print $ gestaltEntropy g'
+          print $ motifIndices g'
