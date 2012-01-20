b700096aeaf40dd59bbf2a677922ea9ae58e358d
Patrick <synapse.and.syntax@gmail.com>
2012-01-20 17:04:24 -0500

    factored out function cmdline argument, numIterations => convergence option

--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,23 @@ import System (getArgs)
 
 main = do args <- getArgs
           let iterations = read $ head args
-          let methodName = args !! 1
-          let f = case methodName of
-                "sa" -> sa
-                "patrify" -> patrify
-                "patrifySweep" -> patrifySweep
+          let functionName = args !! 1
+          let f = interpretFunction functionName
           seqs <- readSequences "../data/lexA_e_coli_120.csv"
           mis <- seedMotifs seqs
           let g = Gestalt seqs mis
-          g' <- iterateN' iterations (>>= f) (return g)
+          g' <- if iterations > 0
+                then iterateN' iterations (>>= f) (return g)
+                else converge g f
+
           print $ gestaltEntropy g'
           print $ motifIndices g'
+          print $ recoverMotifs g'
+
+
+interpretFunction :: String -> (Gestalt -> IO Gestalt)          
+interpretFunction fname = case fname of
+  "sa" -> sa
+  "patrify" -> patrify
+  "patrifySweep" -> patrifySweep
+  "greedy" -> greedy
