de31381d9605a2578a9f7eb22adf871686f75272
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-01-29 19:03:35 -0500

    updated example.hs

--- src/example.hs
+++ src/example.hs
@@ -1,5 +1,5 @@
 seqs <- readSequences "../data/lexA_e_coli_120.csv"
-let mis = replicate 30 [52,0,10] ::[[Int]]
+let mis = replicate 30 [52] ::[[Int]]
 let motifNum = 0 :: Int          
 let seqNum     = 0 :: Int          
 let (mi,mis') = separate motifNum mis
@@ -9,3 +9,4 @@ let muMatrix = meanMatrix mis' :: MeanMatrix
 let varMatrix = varianceMatrix mis' :: VarMatrix
 let statMatrices = (muMatrix, varMatrix)
 let positions = [0..104] :: [Int]
+let g = Gestalt seqs mis
