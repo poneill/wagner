003a30fd78f7f69363e3ce1e067d7a91e44389c7
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-01-31 10:58:50 -0500

    made example.hs legal Haskell code

--- src/example.hs
+++ src/example.hs
@@ -1,12 +1,18 @@
-seqs <- readSequences "../data/lexA_e_coli_120.csv"
-let mis = replicate 30 [52] ::[[Int]]
-let motifNum = 0 :: Int          
-let seqNum     = 0 :: Int          
-let (mi,mis') = separate motifNum mis
-let (seq,seqs') = separate seqNum seqs
-let looPSSM = recoverNthPSSM seqs' mis' motifNum -- revise    
-let muMatrix = meanMatrix mis' :: MeanMatrix
-let varMatrix = varianceMatrix mis' :: VarMatrix
-let statMatrices = (muMatrix, varMatrix)
-let positions = [0..104] :: [Int]
-let g = Gestalt seqs mis
+import Utils
+import Wagner
+main = do 
+  seqs <- readSequences "../data/lexA_e_coli_120.csv"
+  let mis = replicate 30 [52] ::[[Int]]
+  let motifNum = 0 :: Int          
+  let seqNum     = 0 :: Int          
+  let i = seqNum
+  let (mi,mis') = separate motifNum mis
+  let (seq,seqs') = separate seqNum seqs
+  let looPSSM = recoverNthPSSM seqs' mis' motifNum -- revise    
+  let pssm = looPSSM
+  let muMatrix = meanMatrix mis' :: MeanMatrix
+  let varMatrix = varianceMatrix mis' :: VarMatrix
+  let statMatrices = (muMatrix, varMatrix)
+  let positions = [0..104] :: [Int]
+  let g = Gestalt seqs mis
+  return g
